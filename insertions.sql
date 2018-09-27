c711cff86c7f132ac5ee1178429e5bc3f6795297
ncoudin <33418800+ncoudin@users.noreply.github.com>
2018-09-28 00:36:02 +0200

    insertion admin

--- insertions.sql
+++ insertions.sql
@@ -1,5 +1,5 @@
-insert into utilisateur values('roger','roger','dupont','7 rue Oui',77777,'azoiko@akjek.iaz','aaa',1);
-insert into utilisateur (pseudo,mdp) values('gerard','123');
+insert into utilisateur values('admin','coudin','naoto','admin',11111,'admin@admin','123',1);
+/*insert into utilisateur (pseudo,mdp) values('gerard','123');
 
 insert into typeavion values(1,'A380',500),
 							(2,'A320',400);
@@ -10,4 +10,4 @@ insert into aeroport(nomAeroport) values('Charles de Gaulle'),('Orly'),('Lyon'),
 
 insert into vol(avion,aeroport1,aeroport2,dateDepart,dateArrivee,prix) values (1,1,2,'2018-11-06 17:00:00','2018-11-06 20:00:00',120.55);
 
-insert into reservation values ('roger',1,5);
+insert into reservation values ('roger',1,5);*/
